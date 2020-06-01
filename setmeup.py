58328646bc39524bf378a6208fa2721ee831bce1
John Leon <nanodano@devdungeon.com>
2020-06-01 13:01:52 -0500

    Update setmeup.py

--- setmeup.py
+++ setmeup.py
@@ -5,6 +5,8 @@ Setup Zsh config and other dotfiles
 from os import system
 
 # Symlink zsh & vim config
+system('rm $HOME/.zshrc')
+system('rm $HOME/.vimrc')
 system('ln -s $HOME/NanoBin/dotfiles/zshrc $HOME/.zshrc')
 system('ln -s $HOME/NanoBin/dotfiles/vimrc $HOME/.vimrc')
 
